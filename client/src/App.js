8893bd4c33899a3c4679489510a3d722ed3b3c2f
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-05-10 22:23:21 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,36 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [file,setFile]=useState("");
   useEffect(() => {
     axios
-      .get("http://localhost:5000")
+      .get("http://localhost:4050")
       .then((res) => setData(res.data))
       .catch((err) => console.log(err, "it has an error"));
   });
+
+
+  const postImage=()=>{
+    const imageData=new FormData();
+    imageData.set('file',file[0])
+    fetch("http://localhost:4050",{
+      method:"POST",
+      body:imageData
+    })
+    .then((res)=>{
+      return res.json();
+    })
+    .then((json)=>{
+      console.log(json);
+    })
+    
+  }
   return (
     <div className="App">
+      <div>
+        <input type='file' onChange={(e)=>setFile(e.target.files)}/>
+        <button onClick={postImage}>Uploads</button>
+      </div>
       <h1>Image uploading react</h1>
       {data.map((singleData) => {
         const base64String = btoa(
--- server/index.js
+++ server/index.js
@@ -26,23 +26,25 @@ const storage = multer.diskStorage({
   
   const upload = multer({ storage: storage });
   
-  app.post("/", upload.single("file"), (req, res) => {
-    const saveImage =  images({
+  app.post("/", upload.single("file"), async(req, res) => {
+    try{
+        const saveImage = new images({
    
-      img: {
-        data: fs.readFileSync("uploads/" + req.file.filename),
-        contentType: "image/png",
-      },
-    });
-    saveImage
-      .save()
-      .then((res) => {
-        console.log("image is saved");
-      })
-      .catch((err) => {
-        console.log(err, "error has occur");
-      });
-      res.send('image is saved')
+            img: {
+              data: fs.readFileSync("uploads/" + req.file.filename),
+              contentType: "image/png",
+            },
+          })
+          const image=await saveImage.save();
+          res.status(200).json(image);
+
+    }
+    catch(err){
+        res.status(505).json(err);
+
+    }
+   
+   
   });
   
   
